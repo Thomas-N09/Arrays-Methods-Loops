df2a128a1aed303912a089337806ae5669fc89a8
Thomas-N09 <nyth092024@gmail.com>
2024-11-24 16:32:08 +0100

    6-11

--- main.js
+++ main.js
@@ -47,22 +47,46 @@ newArray.forEach((element) => console.log(element));
 // Using the .shift() and .pop() method, remove the first and last item from myArray.
 //print the result
 
+const firstItem = myArray.shift();
+myArray.pop();
+console.log(myArray);
+
 // 7.--------------------------------------------------------------------------------------
 // create a variable and assign it the content of myArray as a string using .join().
 // print your variable.
 //BONUS - See if you can make the items be seperated by a space
 
+// Var veldig usikker her. Er det mulig å legge til text bare ên gang med .join?
+// Når jeg prøvde så ble "strawberry" lagt til hver eneste item.
+
+const fruit = myArray.push("strawberry");
+
+console.log(myArray.join(" "));
+
 //8--------------------------------------------------------------------------------------
 //Create a function that takes a number as an argument and returns that number squared.
 // call your function by using a valid argument
-function numberSquared(number) {}
 
-numberSquared();
+//let number = 4;
+function numberSquared(number) {
+  return Math.pow(number, 2);
+}
+
+console.log(numberSquared(6));
 
 //9.--------------------------------------------------------------------------------------
 //Using the "%" (modulo) operator complete the function,
 // it should take "number" as an argument and return "even" if its even and "odd" if its odd.
-function oddOrEven(number) {}
+function oddOrEven(number) {
+  if (number % 2 === 0) {
+    console.log("even");
+  } else {
+    console.log("odd");
+  }
+}
+
+oddOrEven(7);
+
 //call your function by using one of the options below.
 // oddOrEven(prompt());
 // oddOrEven()
@@ -73,15 +97,37 @@ function oddOrEven(number) {}
 //  c = a + b;
 //  a = b;
 //  b = c;
-function Fibbonacci(number) {}
-Fibbonacci();
+
+function Fibbonacci(number) {
+  let a = 0,
+    b = 1,
+    c;
+  console.log(a);
+  console.log(b);
+  for (let i = 2; i < number; i++) {
+    c = a + b;
+    console.log(c);
+    a = b;
+    b = c;
+  }
+}
+
+Fibbonacci(10);
 
 //11.--------------------------------------------------------------------------------------
 //BONUS!Implement fizzbuzz, if a number is divisible by 3: Fizz, if a number is divisible by 5: Buzz, if the number is divisible by both 3 and 5, FizzBuzz
 // https://www.geeksforgeeks.org/fizz-buzz-implementation/
-function fizzbuzz(number) {}
 
-fizzbuzz();
+function fizzbuzz(number) {
+  for (var i = 1; i <= 15; i++) {
+    if (i % 15 == 0) console.log("FizzBuzz");
+    else if (i % 3 == 0) console.log("Fizz");
+    else if (i % 5 == 0) console.log("Buzz");
+    else console.log(i);
+  }
+}
+
+fizzbuzz(5);
 
 //12.--------------------------------------------------------------------------------------
 //SUPERBONUS - we have not talked about recursion yet, but here is a fun one to try.
